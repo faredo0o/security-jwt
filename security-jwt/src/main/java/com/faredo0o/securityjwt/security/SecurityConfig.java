754998397e927131d65864668b9ff96c2024a501
Essam <mastermail@gmail.com>
2022-05-17 16:27:30 +0200

    after adding jwt AuthenticationFilter

--- security-jwt/src/main/java/com/faredo0o/securityjwt/security/SecurityConfig.java
+++ security-jwt/src/main/java/com/faredo0o/securityjwt/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.faredo0o.securityjwt.security;
+import com.faredo0o.securityjwt.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -21,6 +28,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        CustomAuthenticationFilter customAuthenticationFilter=new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/login/**").permitAll()
+                .antMatchers(GET,"/api/user/**").hasAuthority("ROLE_USER")
+                .antMatchers(POST,"/api/users/save/**").hasAuthority("ROLE_ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(customAuthenticationFilter);
+
+    }
+    @Override
+    @Bean
+   public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 }
